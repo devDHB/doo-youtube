eae3465f59bf438dcfa7e2722ce8e23b2af9a3d4
unknown <myownlife323@naver.com>
2025-02-13 02:51:16 +0900

    suggest video

---
+++ src/data/Today.js
@@ -0,0 +1,49 @@
+import todayImage01 from "../assets/img/today/todayImage01.png";
+import todayImage02 from "../assets/img/today/todayImage02.png";
+import todayImage03 from "../assets/img/today/todayImage03.png";
+import todayImage04 from "../assets/img/today/todayImage04.png";
+import todayImage05 from "../assets/img/today/todayImage05.png";
+
+export const todayText = [
+    {
+        img: todayImage01,
+        title: "How to Build Responsive YouTube Clone using HTML & CSS",
+        desc : "Do you want to improve your #html & #css skills? Then Watch this video and follow along with me where we build #responsive #youtubeclone using just HTML & CSS. We will be using #flexbox and Google font Icons in this project.",
+        author : "Future Coders",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        date: "2021.03.07",
+        page: "/youtube"
+    },{
+        img: todayImage02,
+        title: "Full Stack JavaScript Developer Roadmap 2025",
+        desc : "This is my complete roadmap to become a full stack JavaScript web developer. It takes you from the very begining, learning HTML, CSS and JS to frameworks, to backend and career deevelop.",
+        author : "Traversy Media",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        date: "2025.02.11",
+        page: "/youtube"
+    },{
+        img: todayImage03,
+        title: "React Full Course for free ⚛️ (2024)",
+        desc : "This is a beginners React JS course that should be enough to get you started using the React Library. There are still many more hooks and concepts that are beyond the scope of the beginner level. This video should at least give you a solid foundation.",
+        author : "Bro Code",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        date: "2024.01.17",
+        page: "/youtube"
+    },{
+        img: todayImage04,
+        title: "Blazor Fundamentals Tutorial – Learn Blazor Step-by-Step",
+        desc : "This crash course will guide you through learning the Blazor basics as a beginner, enabling you to start building web applications in .NET 8 from scratch!.",
+        author : "freeCodeCamp.org",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        date: "2023.07.07",
+        page: "/youtube"
+    },{
+        img: todayImage05,
+        title: "100초의 Next.js // 전체 초보자 튜토리얼",
+        desc : "100초 안에 Next.js의 기본을 배워보세요! 그런 다음 전체 Next.js 초보자 튜토리얼을 통해 첫 번째 서버 렌더링 반응 앱을 구축하세요.",
+        author : "Fireship",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        date: "2021.01.06",
+        page: "/youtube"
+    },
+]
--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -1,12 +1,43 @@
-import React from "react";
-import Main from "../components/section/Main";
+import React from 'react'
+import Main from '../components/section/Main'
+
+import { todayText } from '../data/Today'
+import { Link } from 'react-router-dom'
 
 const Today = () => {
-  return (
-    <Main title="Today">
-      Today
-    </Main>
-  )
-};
+    return (
+        <Main 
+            title = "おすすめ動画"
+            description="今日のおすすめ動画です。">
+            
+            <section id='todayPage'>
+                <h2>🥰 今日のおすすめ動画です。</h2>
 
-export default Today;
+                {todayText.map((today, key) => (
+                    <div className='today__inner' key={key}>
+                        <div className='today__thumb play__icon'>
+                            <Link to={today.page}>
+                                <img src={today.img} alt={today.title} />
+                            </Link>
+                        </div>
+                        <div className='today__text'>
+                            <span className='today'>今日のオススメ</span>
+                            <h3 className='title'>
+                                <Link to={today.page}>{today.title}</Link>
+                            </h3>
+                            <p className='desc'>{today.desc}</p>
+                            <div className='info'>
+                                <span className='author'>
+                                    <Link to={`/channel/${today.channelId}`}>{today.author}</Link>
+                                </span>
+                                <span className='date'>{today.date}</span>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </section>
+        </Main>
+    )
+}
+
+export default Today
